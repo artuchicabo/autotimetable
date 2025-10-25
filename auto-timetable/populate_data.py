287ace5fcaef8c416b8a1322002c5ffdcfd0ae84
artuchicabo <markgamer098@gmail.com>
2025-10-25 11:55:58 +0700

    10

--- auto-timetable/populate_data.py
+++ auto-timetable/populate_data.py
@@ -4,26 +4,26 @@ conn = sqlite3.connect('school.db')
 c = conn.cursor()
 
 # ครู
-teachers = ["ครูสมชาย", "ครูสมหญิง", "ครูวิทย์", "ครูภาษาอังกฤษ"]
+teachers = ["นาย พิชญะ พรมลา", "นาย กรรัก พร้อมจะบก", "นางสาวสุวนันท์ กอศรีรมย์", "นาย ประจิตร์ เลขตะระโก","นาวบังอร เลขะตะระโก","นาย นิราชัน กุลชัย"]
 for t in teachers:
     c.execute("INSERT INTO teachers (name) VALUES (?)", (t,))
 
 # ชั้นเรียน
-classes = ["ม.1/1", "ม.1/2"]
+classes = ["ปวส.2/1", "ปวส.2/2"]
 for cl in classes:
     c.execute("INSERT INTO classes (name) VALUES (?)", (cl,))
 
 # ห้องเรียน
-rooms = ["ห้อง A", "ห้อง B"]
+rooms = ["LAB 6405", "LAB 6406", "LAB 6407", "LAB 6408"]
 for r in rooms:
     c.execute("INSERT INTO rooms (name) VALUES (?)", (r,))
 
 # วิชา (teacher_id)
 subjects = [
-    ("คณิตศาสตร์", 1),
-    ("วิทยาศาสตร์", 3),
-    ("ภาษาอังกฤษ", 4),
-    ("ประวัติศาสตร์", 2)
+    ("โครงสร้างและอัลกอริทึม", 1),
+    ("การพัฒซอฟต์แวร์เชิงวัตถุ", 3),
+    ("การออกแบบฐานข้อมูล", 4),
+    ("การเป็นผู้ประกอบการ", 2)
 ]
 for s, teacher_id in subjects:
     c.execute("INSERT INTO subjects (name, teacher_id) VALUES (?, ?)", (s, teacher_id))

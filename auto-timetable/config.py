be34a92c4f645527f03d2e5695e9caf9fe9d7a01
artuchicabo <markgamer098@gmail.com>
2025-11-04 16:38:27 +0700

    a2

--- auto-timetable/config.py
+++ auto-timetable/config.py
@@ -2,4 +2,18 @@ import firebase_admin
 
 from firebase_admin import credentials, db
 
-cred = credentials.Certificate("auto-timetable/firebase-sdk.json")
+cred = credentials.Certificate("config/serviceAccountkey.json")
+
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://autotimetable-382ee-default-rtdb.asia-southeast1.firebasedatabase.app/'
+})
+
+ref = db.reference('teachers')
+ref.push({
+    'name': 'Test Teacher',
+    'subject': 'Mathematics',
+    "available": ["Monday 9am-10am", "Wednesday 1pm-2pm"]
+})
+
+all_teachers = ref.get()
+print("All Teachers:", all_teachers)
